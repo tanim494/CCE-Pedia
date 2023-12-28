f6e8e18b46d5add2bd15a0b31b33226c8ec74b0d
tanim <tanim494@gmail.com>
2023-12-28 18:28:55 +0600

    Fixed some bugs

--- app/src/main/java/com/tanim/ccepedia/WebContent.java
+++ app/src/main/java/com/tanim/ccepedia/WebContent.java
@@ -11,13 +11,12 @@ import androidx.fragment.app.Fragment;
 
 public class WebContent extends Fragment {
     private static String openLink;
-    private WebView webView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_web_content, container, false);
 
-        webView = view.findViewById(R.id.webContent);
+        WebView webView = view.findViewById(R.id.webContent);
         WebSettings webSettings = webView.getSettings();
         webView.canGoBack();
         webSettings.setJavaScriptEnabled(true); // Enable JavaScript if required
