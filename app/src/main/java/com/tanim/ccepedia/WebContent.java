4c52b5c82e0a107718062a2863bf55d29d4f6989
tanim <tanim494@gmail.com>
2023-12-28 20:10:11 +0600

    Updated Bus schedule and Routie UI

--- app/src/main/java/com/tanim/ccepedia/WebContent.java
+++ app/src/main/java/com/tanim/ccepedia/WebContent.java
@@ -1,5 +1,6 @@
 package com.tanim.ccepedia;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,12 +18,22 @@ public class WebContent extends Fragment {
         View view = inflater.inflate(R.layout.fragment_web_content, container, false);
 
         WebView webView = view.findViewById(R.id.webContent);
+        webView.setBackgroundColor(Color.parseColor("#738F73"));
         WebSettings webSettings = webView.getSettings();
-        webView.canGoBack();
         webSettings.setJavaScriptEnabled(true); // Enable JavaScript if required
-        webView.setWebViewClient(new WebViewClient());
-
         webView.loadUrl(openLink);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                // Inject JavaScript to hide elements with a specific class
+                //webView.evaluateJavascript("document.body.style.color='#FFFFFF';", null);
+                //webView.evaluateJavascript("document.querySelectorAll('h1').forEach(function(h1) { h1.style.color='#FFFFFF'; });", null);
+                webView.evaluateJavascript("document.querySelector('.mainTableTopMiddle').style.display='none';", null);
+                webView.evaluateJavascript("document.querySelector('.flex-shrink-0').style.backgroundColor='#738F73';", null);                webView.evaluateJavascript("document.querySelector('.ownershipPanel').style.display='none';", null);
+                webView.evaluateJavascript("document.querySelector('.articleFooterContainer').style.display='none';", null);
+                webView.evaluateJavascript("document.querySelector('.footer').style.display='none';", null);
+            }
+        });
 
         return view;
     }
